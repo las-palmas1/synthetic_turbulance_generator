f4e56673cd19ad589cccd1ea72497f3f98be8025
Alexander <aszhigalkin94@gmail.com>
2017-09-12 22:18:01 +0300

    Create velocity field

--- matlab/lib.py
+++ matlab/lib.py
@@ -1,4 +1,11 @@
 import numpy as np
+from scipy.interpolate import interp1d
+from scipy.fftpack import fftn, ifftn
+import logging
+import config
+import time
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=config.log_level)
 
 
 def make_fft_grid(num: int, length):
@@ -46,3 +53,121 @@ def make_spectrum(u_fft: np.ndarray, v_fft: np.ndarray, w_fft: np.ndarray, num: 
     k_mag = 2 * np.pi / length * np.linspace(0, m_max, m_max + 1)
     e_k_mag = e_k_mag * length / (2 * np.pi)
     return k_mag, e_k_mag
+
+
+def energy(k_mag, k_fit: np.ndarray, e_k_fit: np.ndarray):
+    i = 0
+    num = len(k_fit)
+    e_k = ((k_mag - k_fit[0])/(k_fit[1] - k_fit[0]) * (e_k_fit[1] - e_k_fit[0]) + e_k_fit[0])
+    while k_mag > k_fit[i]:
+        i += 1
+        if i == num:
+            e_k = np.exp((np.log(k_mag) - np.log(k_fit[num-2])) / (np.log(k_fit[num-1]) - np.log(k_fit[num-2])) *
+                         (np.log(e_k_fit[num-1]) - np.log(e_k_fit[num-2])) + np.log(e_k_fit[num-2]))
+            break
+        e_k = np.exp((np.log(k_mag) - np.log(k_fit[i-1])) / (np.log(k_fit[i]) - np.log(k_fit[i-1])) *
+                     (np.log(e_k_fit[i]) - np.log(e_k_fit[i-1])) + np.log(e_k_fit[i-1]))
+    return e_k
+
+
+def make_field(num: int, length: float, m: np.ndarray, k: np.ndarray, k_fit: np.ndarray, e_k_fit: np.ndarray):
+    logging.info('START VELOCITY FIELD GENERATION')
+    start = time.time()
+    dx = length / num
+    dk = 2 * np.pi / length
+    u_hat = np.zeros([num, num, num])
+    v_hat = np.zeros([num, num, num])
+    w_hat = np.zeros([num, num, num])
+
+    kmod = 2j * np.sin(np.pi * m / num) / dx
+
+    for im in range(1, int(num / 2 + 1)):
+        for jm in range(1, num):
+            for km in range(1, num):
+                k_mag = np.sqrt(k[im]**2 + k[jm]**2 + k[km]**2)
+                u_hat_mag = 1 / (dx**3) * np.sqrt(2 * interp1d(k_fit, e_k_fit, bounds_error=False, fill_value=0)(k_mag) *
+                                                  dk / (4 * np.pi * (k_mag/dk)**2))
+                kdiv = np.array([kmod[im], kmod[jm], kmod[km]])
+                theta_rand = 2 * np.pi * np.random.rand(3)
+
+                u1 = np.cross([1.123, -0.982, 1.182], kdiv)
+                u1 = u1 / np.linalg.norm(u1)
+                u2 = np.cross(u1, kdiv)
+                u2 = u2 / np.linalg.norm(u2)
+
+                u_hat_re = u_hat_mag * np.cos(theta_rand[0])
+                u_hat_im = u_hat_mag * np.sin(theta_rand[0])
+
+                r1 = np.cos(theta_rand[1])*u1 + np.sin(theta_rand[1])*u2
+                r2 = np.cos(theta_rand[2])*u1 + np.sin(theta_rand[2])*u2
+
+                u_hat[im, jm, km] = r1[0] * u_hat_re + 1j*r2[0]*u_hat_im
+                v_hat[im, jm, km] = r1[1] * u_hat_re + 1j*r2[1]*u_hat_im
+                w_hat[im, jm, km] = r1[2] * u_hat_re + 1j*r2[2]*u_hat_im
+                logging.info('im = %s, jm = %s, km = %s' % (im, jm, km))
+
+    for im in range(int(num/2 + 1), num):
+        for jm in range(1, num):
+            for km in range(1, num):
+                u_hat[im, jm, km] = u_hat[num-im, num-jm, num-km]
+                v_hat[im, jm, km] = v_hat[num-im, num-jm, num-km]
+                w_hat[im, jm, km] = w_hat[num-im, num-jm, num-km]
+
+    im = int(num / 2)
+    for jm in range(int(num / 2 + 1), num):
+        for km in range(1, num):
+            u_hat[im, jm, km] = u_hat[num - im, num - jm, num - km]
+            v_hat[im, jm, km] = v_hat[num - im, num - jm, num - km]
+            w_hat[im, jm, km] = w_hat[num - im, num - jm, num - km]
+
+    im = int(num / 2)
+    jm = int(num / 2)
+    for km in range(int(num / 2 + 1), num):
+        u_hat[im, jm, km] = u_hat[num - im, num - jm, num - km]
+        v_hat[im, jm, km] = v_hat[num - im, num - jm, num - km]
+        w_hat[im, jm, km] = w_hat[num - im, num - jm, num - km]
+
+    im = int(num / 2)
+    jm = int(num / 2)
+    km = int(num / 2)
+    u_hat[im, jm, km] = 0
+    v_hat[im, jm, km] = 0
+    w_hat[im, jm, km] = 0
+    finish = time.time()
+    logging.info('FINISH VELOCITY FIELD GENERATION')
+    logging.info('Total time  =  %.3f' % (finish - start))
+    return u_hat, v_hat, w_hat
+
+
+def make_fft(u, v, w):
+    u_hat = fftn(u)
+    v_hat = fftn(v)
+    w_hat = fftn(w)
+    u_hat = np.fft.fftshift(u_hat)
+    v_hat = np.fft.fftshift(v_hat)
+    w_hat = np.fft.fftshift(w_hat)
+    return u_hat, v_hat, w_hat
+
+
+def make_ifft(u_hat, v_hat, w_hat):
+    u_hat = np.fft.fftshift(u_hat)
+    v_hat = np.fft.fftshift(v_hat)
+    w_hat = np.fft.fftshift(w_hat)
+    u = ifftn(u_hat)
+    v = ifftn(v_hat)
+    w = ifftn(w_hat)
+    return u.real, v.real, w.real
+
+
+def save_filed(u: np.ndarray, v: np.ndarray, w: np.ndarray, filename: str):
+    ar = np.zeros([3] + list(u.shape))
+    ar[0] = u
+    ar[1] = v
+    ar[2] = w
+    np.save(filename, ar)
+
+
+def load_field(filename: str):
+    result = np.fromfile(filename)
+    return result[0], result[1], result[2]
+
