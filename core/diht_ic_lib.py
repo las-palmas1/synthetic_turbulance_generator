Correct velocity calculation in les_inlet_ic_lib, correct spectrum computation.

core/diht_ic_lib.py
@@ -58,14 +58,14 @@ def make_spectrum_alt_way(u_hat: np.ndarray, v_hat: np.ndarray, w_hat: np.ndarra
     return k_mag, e_k_mag
 
 
-def _get_energy(m_grid: np.ndarray, energy_arr: np.ndarray, m_mag, res):
-    energy_arr_filt = energy_arr[(m_grid > m_mag - 0.5 * res) * (m_grid < m_mag + 0.5 * res)]
-    energy = energy_arr_filt.sum() / res
+def _get_energy(m_grid: np.ndarray, energy_arr: np.ndarray, m_mag):
+    energy_arr_filt = energy_arr[(m_grid > m_mag - 0.5) * (m_grid < m_mag + 0.5)]
+    energy = energy_arr_filt.sum()
     return energy
 
 
 def make_spectrum(num: int, length, u_hat: np.ndarray, v_hat: np.ndarray, w_hat: np.ndarray, m: np.ndarray,
-                  num_pnt=100, res=3):
+                  num_pnt=100):
     logging.info('START CALCULATING SPECTRUM')
     m_i = np.zeros([num, num, num])
     m_j = np.zeros([num, num, num])
@@ -77,11 +77,11 @@ def make_spectrum(num: int, length, u_hat: np.ndarray, v_hat: np.ndarray, w_hat:
                 m_j[im, jm, km] = m[jm]
                 m_k[im, jm, km] = m[km]
     m_grid = np.sqrt(m_i**2 + m_j**2 + m_k**2)
-    energy = 0.5 * (length / num)**6 * (np.abs(u_hat)**2 + np.abs(v_hat)**2 + np.abs(w_hat)**2)
+    energy = 0.5 * (1 / num)**6 * (np.abs(u_hat)**2 + np.abs(v_hat)**2 + np.abs(w_hat)**2)
     m_mag = np.linspace(0, m_grid.max(), num_pnt)
     e_k_mag = np.zeros(num_pnt)
     for i in range(num_pnt):
-        e_k_mag[i] = _get_energy(m_grid, energy, m_mag[i], res) * length / (2 * np.pi)
+        e_k_mag[i] = _get_energy(m_grid, energy, m_mag[i]) * length / (2 * np.pi)
         logging.debug('e_k_mag[%s] = %.5f' % (i, e_k_mag[i]))
     k_mag = m_mag * 2 * np.pi / length
     logging.info('FINISH CALCULATING SPECTRUM')
@@ -120,7 +120,7 @@ def get_max_div(u: np.ndarray, v: np.ndarray, w: np.ndarray, num: int, length):
 def make_field(num: int, length: float, m: np.ndarray, k: np.ndarray, k_fit: np.ndarray, e_k_fit: np.ndarray):
     logging.info('START VELOCITY FIELD GENERATION')
     start = time.time()
-    dx = length / num
+    dx = 1 / num
     dk = 2 * np.pi / length
     u_hat = np.zeros_like(np.zeros([num, num, num]), dtype=np.complex)
     v_hat = np.zeros_like(np.zeros([num, num, num]), dtype=np.complex)
@@ -132,7 +132,7 @@ def make_field(num: int, length: float, m: np.ndarray, k: np.ndarray, k_fit: np.
         for jm in range(1, num):
             for km in range(1, num):
                 k_mag = np.sqrt(k[im]**2 + k[jm]**2 + k[km]**2)
-                u_hat_mag = 1 / (dx**3) * np.sqrt(2 * energy_interp(k_mag, k_fit, e_k_fit) *
+                u_hat_mag = 1 / dx**3 * np.sqrt(2 * energy_interp(k_mag, k_fit, e_k_fit) *
                                                   dk / (4 * np.pi * (k_mag/dk)**2))
                 if np.isnan(u_hat_mag):
                     u_hat_mag = 0
@@ -201,9 +201,9 @@ def make_fft(u, v, w):
 
 
 def make_ifft(u_hat, v_hat, w_hat):
-    u_hat = np.fft.fftshift(u_hat)
-    v_hat = np.fft.fftshift(v_hat)
-    w_hat = np.fft.fftshift(w_hat)
+    u_hat = np.fft.ifftshift(u_hat)
+    v_hat = np.fft.ifftshift(v_hat)
+    w_hat = np.fft.ifftshift(w_hat)
     u = ifftn(u_hat)
     v = ifftn(v_hat)
     w = ifftn(w_hat)

diht_ic/generate_field.py
@@ -1,16 +1,19 @@
-from diht_ic.lib import make_field, make_fft_grid, save_filed, make_ifft, make_spectrum_alt_way, get_max_div, make_spectrum
-from scipy.io import loadmat
+import logging
+
 import matplotlib.pyplot as plt
 import numpy as np
-import logging
+from scipy.io import loadmat
+
 import config
+from core.diht_ic_lib import make_field, make_fft_grid, save_filed, make_ifft, get_max_div, \
+    make_spectrum, make_fft
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=config.log_level)
 
 exp_data = loadmat(r'C:\Users\User\Documents\tasks\others\ic_gen\CBC_exp.mat')
 
-num = 32
-length = 1
+num = 50
+length = 1.9
 
 M = 5.08
 U0 = 1000
@@ -30,11 +33,12 @@ if __name__ == '__main__':
     v_hat = velocities[1]
     w_hat = velocities[2]
     u, v, w = make_ifft(u_hat, v_hat, w_hat)
+    uh, vh, wh = make_fft(u, v, w)
     divmax = get_max_div(u, v, w, num, length)
     logging.info('MAX DIVERGENCE = %.2f' % divmax)
     save_filed(u, v, w, 'velocity_filed')
 
-    k_mag, e_k_mag = make_spectrum(num, length, u_hat, v_hat, w_hat, m, num_pnt=100, res=2)
+    k_mag, e_k_mag = make_spectrum(num, length, uh, vh, wh, m, num_pnt=100)
     plt.figure(figsize=(8, 6))
     plt.plot(k_mag, e_k_mag, lw=0.5, label='Synthetic field')
     plt.plot(k_42, E_42, lw=0.5, label='Experiment')

spectrum/spectrum_lib.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fftpack import fftn
 import config
-from core.lib import get_k_arr, get_tau, get_von_karman_spectrum, get_amplitude_arr, get_d_vector_theta_and_phase, \
-    get_frequency, get_sigma_vector
+from core.les_inlet_ic_lib import get_k_arr, get_tau, get_von_karman_spectrum, get_amplitude_arr, get_d_vector, get_phase, get_z, \
+    get_theta, get_frequency, get_sigma_vector
 from scipy.interpolate import interp1d
 from scipy.integrate import quad
+from core.diht_ic_lib import make_fft_grid, make_fft
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=config.log_level)
 
@@ -23,112 +24,71 @@ def read_velocity_file(filename):
 
 
 class SpatialSpectrum3d:
-    def __init__(self, i_cnt: int, j_cnt: int, k_cnt: int, grid_step: float, u_arr: np.ndarray,
-                 v_arr: np.ndarray, w_arr: np.ndarray, num_point: int, l_e: float =config.l_e,
-                 alpha: float = config.alpha):
-        self.i_cnt = i_cnt
-        self.j_cnt = j_cnt
-        self.k_cnt = k_cnt
+    def __init__(self, num: int, grid_step: float, u_arr: np.ndarray, v_arr: np.ndarray, w_arr: np.ndarray,
+                 num_point: int):
+        """
+        :param num: количество узлов в на стороне генерируемого куба
+        :param grid_step: шаг сетки
+        :param u_arr:
+        :param v_arr:
+        :param w_arr:
+        :param num_point: размер массива со значениями энергии
+        """
+        self.num = num
         self.grid_step = grid_step
         self.u_arr = u_arr
         self.v_arr = v_arr
         self.w_arr = w_arr
         self.num_point = num_point
-        self.l_e = l_e
-        self.alpha = alpha
         self.l_cut = 2 * self.grid_step
-        self.k_abs_arr = None
-        self.energy_sum_arr = None
-        self.energy_u_arr = None
-        self.energy_v_arr = None
-        self.energy_w_arr = None
+        self.u_hat = None
+        self.v_hat = None
+        self.w_hat = None
+        self.k_mag: np.ndarray = None
+        self.e_k_mag: np.ndarray = None
 
     @classmethod
-    def _get_array_3d(cls, array_1d: np.ndarray, i_cnt: int, j_cnt: int, k_cnt: int):
-        result = array_1d.reshape([k_cnt, j_cnt, i_cnt])
-        return result
+    def _get_energy(cls, m_grid: np.ndarray, energy_arr: np.ndarray, m_mag):
+        energy_arr_filt = energy_arr[(m_grid > m_mag - 0.5) * (m_grid < m_mag + 0.5)]
+        energy = energy_arr_filt.sum()
+        return energy
 
     @classmethod
-    def _get_k_abs(cls, l_e: float, l_cut: float, alpha):
-        k_abs_arr = get_k_arr(l_e, l_cut, alpha)
-        k_abs_min = k_abs_arr.min()
-        k_abs_max = k_abs_arr.max()
-        return k_abs_min, k_abs_max
-
-    @classmethod
-    def _get_wave_number_array(cls, i_cnt: int, j_cnt: int, k_cnt: int, grid_step: float, k_abs_min: float,
-                               k_abs_max: float):
-        logging.info('Wave number arrays calculating')
-        x_size = grid_step * (i_cnt - 1)
-        y_size = grid_step * (j_cnt - 1)
-        z_size = grid_step * (k_cnt - 1)
-        result_i = np.zeros([k_cnt, j_cnt, i_cnt])
-        result_j = np.zeros([k_cnt, j_cnt, i_cnt])
-        result_k = np.zeros([k_cnt, j_cnt, i_cnt])
-        # k_max = k_abs_max / np.sqrt(3)
-        # norm_coef = k_abs_min / np.sqrt(1 / x_size ** 2 + 1 / y_size ** 2 + 1 / z_size ** 2)
-        # ki_min = norm_coef / x_size
-        # kj_min = norm_coef / y_size
-        # kk_min = norm_coef / z_size
-        # ki_arr = np.linspace(ki_min, k_max, i_cnt)
-        # kj_arr = np.linspace(kj_min, k_max, j_cnt)
-        # kk_arr = np.linspace(kk_min, k_max, k_cnt)
-        # ki_arr = np.linspace(ki_min, k_max, i_cnt)
-        # kj_arr = np.linspace(kj_min, k_max, j_cnt)
-        # kk_arr = np.linspace(kk_min, k_max, k_cnt)
-        ki_min = 1 / x_size
-        kj_min = 1 / y_size
-        kk_min = 1 / z_size
-        ki_arr = np.linspace(ki_min, i_cnt * ki_min, i_cnt)
-        kj_arr = np.linspace(kj_min, j_cnt * kj_min, j_cnt)
-        kk_arr = np.linspace(kk_min, k_cnt * kk_min, k_cnt)
-        for k in range(k_cnt):
-            for j in range(j_cnt):
-                for i in range(i_cnt):
-                    result_i[k, j, i] = ki_arr[i]
-                    result_j[k, j, i] = kj_arr[j]
-                    result_k[k, j, i] = kk_arr[k]
-        return result_i, result_j, result_k
-
-    @classmethod
-    def _get_energy(cls, k_abs_arr: np.ndarray, velocity_fourier_arr: np.ndarray,
-                    wave_number_abs: float):
-        volume = velocity_fourier_arr.shape[0] * velocity_fourier_arr.shape[1] * velocity_fourier_arr.shape[2]
-        array_for_summation = 0.5 * (np.abs(velocity_fourier_arr) / volume) ** 2
-        array_for_summation_filtered = np.abs(array_for_summation)[(k_abs_arr > wave_number_abs - 0.5) *
-                                                                   (k_abs_arr < wave_number_abs + 0.5)]
-        result = array_for_summation_filtered.sum()
-        logging.debug('wave number = %.4f,   energy = %.4f' % (wave_number_abs, result))
-        return result
-
-    @classmethod
-    def _get_spectrum(cls, ki_arr: np.ndarray, kj_arr: np.ndarray, kk_arr: np.ndarray, velocity_arr: np.ndarray,
-                      num_point: int):
-        logging.info('')
-        logging.info('SPECTRUM CALCULATING\n')
-        k_abs_arr = np.sqrt(ki_arr ** 2 + kj_arr ** 2 + kk_arr ** 2)
-        k_arr_for_spectrum = np.linspace(k_abs_arr.min(), k_abs_arr.max(), num_point)
-        velocity_fourier_arr = fftn(velocity_arr)
-        energy_arr = np.zeros([num_point])
-        for n, k in enumerate(k_arr_for_spectrum):
-            energy_arr[n] = cls._get_energy(k_abs_arr, velocity_fourier_arr, k)
-        return energy_arr, k_arr_for_spectrum
+    def make_spectrum(cls, num: int, length, u_hat: np.ndarray, v_hat: np.ndarray, w_hat: np.ndarray, m: np.ndarray,
+                      num_pnt=100, res=3):
+        logging.info('START CALCULATING SPECTRUM')
+        m_i = np.zeros([num, num, num])
+        m_j = np.zeros([num, num, num])
+        m_k = np.zeros([num, num, num])
+        for im in range(num):
+            for jm in range(num):
+                for km in range(num):
+                    m_i[im, jm, km] = m[im]
+                    m_j[im, jm, km] = m[jm]
+                    m_k[im, jm, km] = m[km]
+        m_grid = np.sqrt(m_i ** 2 + m_j ** 2 + m_k ** 2)
+        energy = 0.5 * (1 / num) ** 6 * (np.abs(u_hat) ** 2 + np.abs(v_hat) ** 2 + np.abs(w_hat) ** 2)
+        m_mag = np.linspace(0, m_grid.max(), num_pnt)
+        e_k_mag = np.zeros(num_pnt)
+        for i in range(num_pnt):
+            e_k_mag[i] = cls._get_energy(m_grid, energy, m_mag[i]) * length / (2 * np.pi)
+            logging.debug('e_k_mag[%s] = %.5f' % (i, e_k_mag[i]))
+        k_mag = m_mag * 2 * np.pi / length
+        logging.info('FINISH CALCULATING SPECTRUM')
+        return k_mag, e_k_mag
 
     def compute_spectrum(self):
-        k_abs_min, k_abs_max = self._get_k_abs(self.l_e, self.l_cut, self.alpha)
-        ki_arr, kj_arr, kk_arr = self._get_wave_number_array(self.i_cnt, self.j_cnt, self.k_cnt, self.grid_step,
-                                                             k_abs_min, k_abs_max)
-        u_arr_3d = self._get_array_3d(self.u_arr, self.i_cnt, self.j_cnt, self.k_cnt)
-        v_arr_3d = self._get_array_3d(self.v_arr, self.i_cnt, self.j_cnt, self.k_cnt)
-        w_arr_3d = self._get_array_3d(self.w_arr, self.i_cnt, self.j_cnt, self.k_cnt)
-        self.energy_u_arr, self.k_abs_arr = self._get_spectrum(ki_arr, kj_arr, kk_arr, u_arr_3d, self.num_point)
-        self.energy_v_arr = self._get_spectrum(ki_arr, kj_arr, kk_arr, v_arr_3d, self.num_point)[0]
-        self.energy_w_arr = self._get_spectrum(ki_arr, kj_arr, kk_arr, w_arr_3d, self.num_point)[0]
-        self.energy_sum_arr = self.energy_u_arr + self.energy_v_arr + self.energy_w_arr
+        u_3d = self.u_arr.reshape([self.num, self.num, self.num])
+        v_3d = self.v_arr.reshape([self.num, self.num, self.num])
+        w_3d = self.w_arr.reshape([self.num, self.num, self.num])
+        self.u_hat, self.v_hat, self.w_hat = make_fft(u_3d, v_3d, w_3d)
+        n, m, x, k = make_fft_grid(self.num, self.grid_step * (self.num - 1))
+        self.k_mag, self.e_k_mag = self.make_spectrum(self.num, self.grid_step * (self.num - 1), self.u_hat,
+                                                      self.v_hat, self.w_hat, m, self.num_point)
 
     def get_turb_kinetic_energy(self):
-        energy_interp = interp1d(self.k_abs_arr, self.energy_sum_arr)
-        result = quad(lambda k: energy_interp(k), min(self.k_abs_arr), max(self.k_abs_arr))[0]
+        energy_interp = interp1d(self.k_mag, self.e_k_mag)
+        result = quad(lambda k: energy_interp(k), min(self.k_mag), max(self.k_mag))[0]
         return result
 
 
@@ -142,7 +102,10 @@ def plot_spectrum_with_predefined(k_arr, energy_arr, filename, l_cut, l_e, l_cut
     tau = get_tau(u0, l_e_max)
     energy_arr_von_karman = get_von_karman_spectrum(k_arr_predef, l_cut, l_e, viscosity, dissipation_rate)
     amplitude_arr = get_amplitude_arr(k_arr_predef, energy_arr_von_karman)
-    d_vector, theta, phase = get_d_vector_theta_and_phase(k_arr_predef.shape[0])
+    z = get_z(k_arr_predef.shape[0])
+    phase = get_phase(k_arr_predef.shape[0])
+    theta = get_theta(z)
+    d_vector = get_d_vector(z, phase, theta)
     frequency = get_frequency(k_arr_predef.shape[0])
     v_arr = []
     for i in range(k_arr_predef.shape[0]):